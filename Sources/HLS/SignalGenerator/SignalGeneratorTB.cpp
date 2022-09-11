d38dfc796a9ae3248ee6eb50947f6d64b9bde39d
John Sloan <jsloan256@gmail.com>
2022-09-11 09:11:58 -0500

    Corrected RadiansPerSample calculation and added amplitude and frequecy checks for the output data.

--- Sources/HLS/SignalGenerator/SignalGeneratorTB.cpp
+++ Sources/HLS/SignalGenerator/SignalGeneratorTB.cpp
@@ -5,7 +5,7 @@
 void CalculateSignalGeneratorControlRegisters(float Frequency, float Vp, SignalGeneratorControlRegistersT<float, uint32_t>* AXI4Control)
 {
   AXI4Control->Vp = Vp;
-  AXI4Control->RadiansPerSample = ((Frequency / SAMPLE_FREQUENCY) * M_PI);
+  AXI4Control->RadiansPerSample = ((Frequency/SAMPLE_FREQUENCY) * (2*M_PI));
 
   printf("Setting Vp to %f\n", Vp, AXI4Control->Vp);
   printf("Setting frequency to %f Hz (%f RadiansPerSample)\n", Frequency, AXI4Control->RadiansPerSample);
@@ -25,13 +25,18 @@ void CalculateSignalGeneratorControlRegisters(float Frequency, float Vp, SignalG
 
 int main()
 {
-  int i;
   int err = 0;
+  int Ndx;
   SignalGeneratorControlRegistersT<float,uint32_t> AXI4Control;
   SignalGeneratorControlRegistersT<SigGenT, ap_uint<1> > Control;
   hls::stream<SigGenAXIS> Output;
   SigGenAXIS OutVal;
   #pragma HLS STREAM variable=Output depth=SIMULATION_LENGTH
+  int PeakNdx1 = 0;
+  int PeakNdx2 = 0;
+  float MaxOutVal = -999.9;
+  float MinOutVal = 999.9;
+  float OutputFrequency;
 
   float Frequency = 4000000.f;  // 4 MHz
   float Vp = 6.f;               // Range will be -6.0 to 6.0
@@ -43,27 +48,52 @@ int main()
   Control.Vp               = (SigGenT) AXI4Control.Vp;
 
   printf("\nRunning UUT\n");
-  for (i=0; i<SIMULATION_LENGTH/FRAMESIZE; i++)
+  for (Ndx=0; Ndx<SIMULATION_LENGTH/FRAMESIZE; Ndx++)
   {
     SignalGeneratorSyn(&Control, Output);
   }
 
-  // TODO: Add output data checking
-  printf("OutVal\n");
+  // Check the Output data stream
+  Ndx = 0;
   while (!Output.empty())
   {
     OutVal = Output.read();
-    printf("%f\n", OutVal.data.to_float());
+
+    // Store that minimum and maximum values of OutVal.data to be compared against the Vp (later)
+    if (OutVal.data.to_float() > MaxOutVal)
+    {
+      MaxOutVal = OutVal.data.to_float();
+    }
+    else if (OutVal.data.to_float() < MinOutVal)
+    {
+      MinOutVal = OutVal.data.to_float();
+    }
+
+    // Record the last two Ndx values where OutVal.data == Vp to calculate the period of OutVal(s)
+    if (OutVal.data.to_float() == Vp)
+    {
+      PeakNdx2 = PeakNdx1;
+      PeakNdx1 = Ndx;
+    }
+
+    // printf("%f\n", OutVal.data.to_float());
+    Ndx++;
   }
 
-  SigGenT temp;
+  // Report an error if the maximum or minimum OutVal doesn't equal the fabs (ignoring signs, of course)
+  if ((fabs(MinOutVal) != Vp) || (MaxOutVal != Vp))
+  {
+    printf("ERROR: Vp check failed (Vp = %f, MinOutVal = %f, MaxOutVal=%f\n", Vp, MinOutVal, MaxOutVal);
+    err++;
+  }
 
-  temp.range() = 0x8000;
-  printf("temp = %f\n", temp.to_float());
-  temp.range() = 0x7FFF;
-  printf("temp = %f\n", temp.to_float());
-
-  err = 0;
+  // Report an error if the output frequency doesn't match the frequency selected
+  OutputFrequency = SAMPLE_FREQUENCY / (PeakNdx1 - PeakNdx2);
+  if (OutputFrequency != Frequency)
+  {
+    printf("ERROR: Frequency check failed (Frequency = %f, OutputFrequency = %f)\n", Frequency, OutputFrequency);
+    err++;
+  }
 
   return err;
 }
